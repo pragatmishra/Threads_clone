145a1556715f6c44ebb927e3aac815ef9d35ade4
pragatmishra <119884277+pragatmishra@users.noreply.github.com>
2025-05-20 21:41:26 +0530

    Update service.js

--- client/src/redux/service.js
+++ client/src/redux/service.js
@@ -10,7 +10,7 @@ import {
 export const serviceApi = createApi({
   reducerPath: "serviceApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://threads-clone-ochre-ten.vercel.app/`,
+    baseUrl: `https://threads-clone-ochre-ten.vercel.app/api`,
     credentials: "include",
   }),
   keepUnusedDataFor: 60 * 60 * 24 * 7,
