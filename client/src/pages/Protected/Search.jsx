38e4a1d489546d12f03573a32e4a266eab81c452
pragatmishra <119884277+pragatmishra@users.noreply.github.com>
2025-05-20 21:06:13 +0530

    Update Search.jsx

--- client/src/pages/Protected/Search.jsx
+++ client/src/pages/Protected/Search.jsx
@@ -1,5 +1,5 @@
 import { Stack, Typography } from "@mui/material";
-import ProfileBar from "../../components/search/ProfileBar";
+import ProfileBar from "../../components/search/Profilebar";
 import SearchInput from "../../components/search/SearchInput";
 import { useSelector } from "react-redux";
 
