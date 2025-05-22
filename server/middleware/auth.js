5ee8a700ce044d0bf3dbfe3d4d6df78c4a35cc34
pragatmishra <119884277+pragatmishra@users.noreply.github.com>
2025-05-22 17:27:50 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,20 +7,23 @@ const auth = async (req, res, next) => {
     if (!token) {
       return res.status(400).json({ msg: "No token in auth !" });
     }
+
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     if (!decodedToken) {
-      return res
-        .status(400)
-        .json({ msg: "Error while decoding token in auth !" });
+      return res.status(400).json({ msg: "Error while decoding token in auth !" });
     }
-    const user = await User.findById(decodedToken.token)
+
+    // Use the correct key from the decoded token payload
+    const user = await User.findById(decodedToken.id)
       .populate("followers")
       .populate("threads")
       .populate("replies")
       .populate("reposts");
+
     if (!user) {
       return res.status(400).json({ msg: "No user found !" });
     }
+
     req.user = user;
     next();
   } catch (err) {
