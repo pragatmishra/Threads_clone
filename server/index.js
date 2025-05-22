88a7644b1f20b2b738ea02b0b40f6b8c8e1971dc
pragatmishra <119884277+pragatmishra@users.noreply.github.com>
2025-05-22 11:29:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`App is listening on PORT : ${port}`);
 });
