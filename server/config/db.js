a729a48489830fe72e2f2970719943baca62c067
pragatmishra <119884277+pragatmishra@users.noreply.github.com>
2025-05-22 12:52:23 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,8 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("MongoDB Connected!");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB Connected...");
 };
 
-export default connectDB;
+module.exports = connectDB;
